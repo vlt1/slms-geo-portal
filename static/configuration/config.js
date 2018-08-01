44f69fdadc30d96f713cc6c145b601365b3c3003
Lorenzo Pini <lore.pini@tin.it>
2018-08-01 11:12:51 +0200

    Gonini specific configuration

--- static/configuration/config.js
+++ static/configuration/config.js
@@ -1,12 +1,14 @@
 window.globalConf = {
-  languages: [{ id: 'en', 'label': 'English' }, { id: 'fr', 'label': 'Français' }],
+  languages: [
+    { id: 'en', 'label': 'English' },
+    { id: 'nl', 'label': 'Nederlands' },
+    { id: 'srn', 'label': 'Sranan Tongo' }],
   map: {
     bingMapsKey: 'ArXO9BbbCsJXDlX8dLLxcI5BhFPTPrKBdg9o-BNWvC8kJFTetsc96izVu0415SDy',
-    mapCenterLonLat: [21.7587, -4.0383],
-    mapZoom: 5,
+    mapCenterLonLat: [-56.6235353, 4.008795],
+    mapZoom: 7,
     defaultGeoServerURLs: [
-      'http://rdc-snsf.org/diss_geoserver/wms',
-      'http://178.33.8.115/diss_geoserver/wms'
+      'https://gonini.org/geoserver/wms'
     ]
   },
   layersConfigApi: {
@@ -18,7 +20,7 @@ window.globalConf = {
     restoreVersionUrl: 'protected/layers_conf/restore_version'
   },
   feedbackApi: {
-    feedbackUrl: 'http://168.195.218.144/feedback',
+    feedbackUrl: 'https://gonini.org/feedback',
     feedbackCategories: [
       'Thematic mapping and Monitoring',
       'Forest Offences',
@@ -34,20 +36,15 @@ window.globalConf = {
         size: { height: 35 }
       },
       {
-        imgUrl: '/static/configuration/logos/inpe.png',
-        link: 'http://www.inpe.br/ingles/',
-        size: { height: 35 }
-      },
-      {
-        text: 'Text link',
-        link: 'http://uxmyths.com/post/715009009/myth-icons-enhance-usability',
+        text: '@Goninigeoportaal',
+        link: 'https://www.facebook.com/Goninigeoportaal/',
         size: { height: 35 }
       }
     ]
   },
   wfsSearchConfig: {
-    layersToQuery: ['ws1:layer1', 'ws1:layer2', 'ws2:layer3'],
-    attributesToFilter: ['attributeOfLayer1', 'anotherAttribute', 'ChangeThisValues'],
+    layersToQuery: ['gonini:infrastructuur_table', 'gonini:terreinen', 'gonini:settlements'],
+    attributesToFilter: ['str_naam', 'cc_code', 'district'],
     baseURL: 'geoserver/ows'
   }
 }
