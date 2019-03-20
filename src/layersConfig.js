862de3e862361cd0dab1c2fc72a26302c66c800c
Lorenzo Pini <lorenzo.pini@geo-solutions.it>
2019-03-20 18:10:07 +0100

    Data Download pages on layers configuration

--- src/layersConfig.js
+++ src/layersConfig.js
@@ -62,6 +62,8 @@ export class Layer {
 
         return ret
       })
+
+      this.downloadLinks = layerConfig.downloadLinks
     }
 
     this.visible = !!layerConfig.visible !== false
@@ -160,7 +162,7 @@ class _Config {
     // Delete the contexts that are not in a group
     this.contexts = contexts.filter(c => this.groups.findById(c.id))
 
-    // Eelete the layers that are not in a context
+    // Delete the layers that are not in a context
     this.layers = layers.filter(l => this.contexts.some(c => c.layers.indexOf(l) !== -1))
 
     // // Mark layers that are not in a context (they will not be instantiated as OL layers)
